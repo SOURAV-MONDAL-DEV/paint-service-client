f3f88481c5de0f61af46548ff07d83dea22c9763
SOURAVMONDAL-BD <souravmondalcode@gmail.com>
2022-11-08 13:22:14 +0600

    setup header footer

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>this is HOME</h1>
+            <h1>this is HOME</h1>
+            <h1>this is HOME</h1>
+            <h1>this is HOME</h1>
+            <h1>this is HOME</h1>
+            <h1>this is HOME</h1>
+        </div>
+    );
+};
+
+export default Home;
